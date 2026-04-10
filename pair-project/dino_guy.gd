476cf399ee5f8677a2ecaeecd7e840b69f58392d
GhostySphinx <fsmithud@udel.edu>
2026-04-10 14:44:52 -0400

    4/10

--- pair-project/dino_guy.gd
+++ pair-project/dino_guy.gd
@@ -1,35 +1,32 @@
 extends CharacterBody2D
 
-var nearby_interactables = []
+@export var nearby_interactables = []
 	
+@export var speed: float = 300.0
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
-func _physics_process(_delta: float) -> void:
-	# Add the gravity.
-	# if not is_on_floor():
-		# velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("move_up", "move_down", "move_right", "move_left")
+func _physics_process(_delta: float):
+	velocity = Vector2.ZERO
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
 	if direction:
 		velocity = direction.normalized() * SPEED
+	if Input.is_action_just_pressed("interact"):
+			if nearby_interactables:
+					nearby_interactables.back().interact()
 
 	move_and_slide()
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
+
+func _on_interaction_detector_area_entered(area):
 	print("interactable detected")
 	area.set_active(true)
 	nearby_interactables.append(area)
 
-func _on_area_2d_area_exited(area: Area2D) -> void:
+func _on_interaction_detector_area_exited(area):
 	print("interactable removed")
 	area.set_active(false)
 	nearby_interactables.erase(area)
---
+++ pair-project/interactable.gd
@@ -0,0 +1,16 @@
+class_name Interactable extends Area2D
+
+@export var interaction_name = "test interaction"
+@export var interation_type = "test"
+@export var display_active = false
+
+func interact():
+		push_warning("This Interactable has no interact() code yet!")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
