bd155e34f47aa7df4c4683e94f3e75f9cac9a4e4
cassidyac <cassanne@udel.edu>
2026-04-25 17:42:54 -0400

    planet 1 on home screen when hovered and interacted takes to planet 1

---
+++ pair-project/Scenes/planet_1.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var player_in_range = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		get_tree().change_scene_to_file("res://Scenes/Planet1.tscn")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		player_in_range = true
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		player_in_range = false
